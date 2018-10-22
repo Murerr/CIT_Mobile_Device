2558500f7406c3ffbf03570f29e0f23280b5d2b2
murerr <rudy.murer@cansii.com>
2018-10-22 23:04:54 +0100

    "adding third activity and colorpicker dialog"

--- app/src/main/java/murer/rudy/soft7035_mobile_assignement/MainActivity.java
+++ app/src/main/java/murer/rudy/soft7035_mobile_assignement/MainActivity.java
@@ -13,20 +13,37 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    static String EXTRA_MESSAGE = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent intentgetData = getIntent();
+        String message = intentgetData.getStringExtra(EXTRA_MESSAGE);
+
+
+
         Button mSecondActivityButton = (Button) findViewById(R.id.mSecondActivityButton);
+        Button mThirdActivityButton = (Button) findViewById(R.id.mThirdActivityButton);
         Button mTranslateButton = (Button) findViewById(R.id.mTranslateButton);
         TextView mTranslateTextView = (TextView) findViewById(R.id.mTranslation);
+        TextView mDataTextView = (TextView) findViewById(R.id.mData);
+        EditText mInputText = (EditText) findViewById(R.id.mInputText);
 
         mSecondActivityButton.setOnClickListener((View v) -> {
             Intent intent = new Intent(this, SecondActivity.class);
+            intent.putExtra(EXTRA_MESSAGE, mInputText.getText());
             startActivity(intent);
         });
+
+        mThirdActivityButton.setOnClickListener((View v) -> {
+            Intent intent = new Intent(this, ThirdActivity.class);
+            intent.putExtra(EXTRA_MESSAGE, mInputText.getText());
+            startActivity(intent);
+        });
+        mDataTextView.setText(message);
     }
 
 }
--- app/src/main/java/murer/rudy/soft7035_mobile_assignement/SecondActivity.java
+++ app/src/main/java/murer/rudy/soft7035_mobile_assignement/SecondActivity.java
@@ -1,11 +1,17 @@
 package murer.rudy.soft7035_mobile_assignement;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static murer.rudy.soft7035_mobile_assignement.MainActivity.EXTRA_MESSAGE;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -13,17 +19,48 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(EXTRA_MESSAGE);
+
+        TextView mDataPassedTextview = (TextView) findViewById(R.id.mDataPassedTextview);
+        Button mGenerateDialog = (Button) findViewById(R.id.mGenerateDialog);
+
+        mDataPassedTextview.setText(message);
+        mGenerateDialog.setOnClickListener((View v) -> {
+            Dialog mPickAColorDialog = CreateDialogColorPicker(savedInstanceState);
+            mPickAColorDialog.show();
         });
+
     }
+
+    private Dialog CreateDialogColorPicker(Bundle savedInstanceState) {
+        AlertDialog.Builder builderSingle = new AlertDialog.Builder(this);
+        builderSingle.setTitle(R.string.pick_color)
+
+        .setPositiveButton(R.string.ok, (dialog, id) -> {
+            // User clicked OK, so save the mSelectedItems results somewhere
+            // or return them to the component that opened the dialog
+        })
+                .setNegativeButton(R.string.cancel, (dialog, id) -> dialog.dismiss());
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.select_dialog_singlechoice);
+        arrayAdapter.add(getResources().getString(R.string.Color_Red));
+        arrayAdapter.add(getResources().getString(R.string.Color_Green));
+        arrayAdapter.add(getResources().getString(R.string.Color_Blue));
+
+        builderSingle.setAdapter(arrayAdapter, (dialog, which) -> {
+            String strName = arrayAdapter.getItem(which);
+            AlertDialog.Builder builderInner = new AlertDialog.Builder(SecondActivity.this);
+            builderInner.setMessage(strName);
+            builderInner.setTitle(R.string.color_picked);
+            builderInner.setPositiveButton("Ok", (dialog1, which1) -> dialog1.dismiss());
+            builderInner.show();
+        });
+
+        return builderSingle.create();
+    }
+
+
 
 }
